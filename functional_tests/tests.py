027e700051ede2c17e972a3c6b0512750cc721a7
WisherBG <wisher@abv.bg>
2017-09-01 16:50:59 +0300

    Refactor functional_tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,9 @@ from django.test import LiveServerTestCase
 import unittest
 import time
 
+
+MAX_WAIT = 10
+
 class NewVisitorTest(LiveServerTestCase):
 
   def setUp(self):
@@ -11,6 +14,12 @@ class NewVisitorTest(LiveServerTestCase):
 
   def tearDown(self):
     self.browser.quit()
+
+  def check_for_row_in_a_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text, [row.text for row in rows])
+
 
   def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -39,10 +48,6 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
 
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly"
     inputbox = self.browser.find_element_by_id('id_new_item')
@@ -51,10 +56,9 @@ class NewVisitorTest(LiveServerTestCase):
     time.sleep(1)
     
     # The page updates itself and now shows both items on her list
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+    self.check_for_row_in_a_list_table('1: Buy peacock feathers')
+    self.check_for_row_in_a_list_table('2: Use peacock feathers to make a fly')
+
 
     self.fail('Finish the test!')
 
