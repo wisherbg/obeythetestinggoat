18168ba254980bb8881a9b2246b5e7a436969f7a
WisherBG <wisher@abv.bg>
2017-09-06 21:14:43 +0300

    Added list model

--- lists/models.py
+++ lists/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class List(models.Model):
+  objects = models.Manager()
+  pass
+  
 class Item(models.Model):
   text = models.TextField(default='')
   objects = models.Manager()
+  list = models.ForeignKey(List, default=None)
+
